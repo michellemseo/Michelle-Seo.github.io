c339a52a4da896f4ad06a8eda2f4f4bfae6a8713
michellemseo <112968364+michellemseo@users.noreply.github.com>
2023-02-21 01:29:44 -0500

    update

--- project1/roof.js
+++ project1/roof.js
@@ -2,10 +2,10 @@ var circle = document.getElementById("box1");
 var text = document.getElementById("one");
 
 circle.onclick = function () {
-    if (text.style.display !== 'none') {
-        text.style.display = 'none';
+    if (text.style.display !== 'block') {
+        text.style.display = 'block';
     } 
     else {
-        text.style.display = "block";
+        text.style.display = "none";
     }
 };
